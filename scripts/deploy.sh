d6f3800d6b94593a683740683993317455ae05ab
Erwan Granger <32554862+erwangranger@users.noreply.github.com>
2023-06-19 15:28:12 -0400

    forgot those.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@
 redis-server /etc/redis/redis.conf &
 
 # Start the API
-cd api && uvicorn src.serge.main:app --host 0.0.0.0 --port 8008 &
+cd api && uvicorn src.serge.main:app --host 0.0.0.0 --port 8080 &
 
 # Wait for any process to exit
 wait -n
--- scripts/dev.sh
+++ scripts/dev.sh
@@ -10,7 +10,7 @@ pip install llama-cpp-python==0.1.62
 redis-server /etc/redis/redis.conf &
 
 # Start the web server
-cd web && npm run dev -- --host 0.0.0.0 --port 8008 &
+cd web && npm run dev -- --host 0.0.0.0 --port 8080 &
 
 # Start the API
 cd api && uvicorn src.serge.main:api_app --reload --host 0.0.0.0 --port 9124 --root-path /api/ &
